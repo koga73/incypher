aca326cf1af25d390c8abf74fe450b111b9944bb
AJ Savino <koga73@gmail.com>
2023-01-10 22:08:02 -0500

    built out cli methods with functionality

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+const DEBUG = false;
+const EXAMPLE_NAME = "ravencoin";
+const FILENAME_REGEX = /^.*?\.?([^\/\\]+)$/; //https://regex101.com/r/MWeJOz/2
+
 const fs = require("fs");
 const homeDir = require("os").homedir();
 const readline = require("readline").createInterface({
@@ -26,47 +30,97 @@ async function run() {
 		throw new Error(`${homeDir} is not accessible`);
 	}
 	const filePath = `${homeDir}/.${packageName}`;
-	const isFileExistent = _fileExists(filePath);
+	const isFileExistent = await _fileExists(filePath);
 	if (isFileExistent) {
 		fileData = await fs.promises.readFile(filePath);
 	} else {
 		await fs.promises.writeFile(filePath, "");
 	}
 
+	//Create zip instance
+	const zip = new Zip({debug: DEBUG});
+	if (fileData !== "") {
+		await zip.setStream(fileData);
+	}
+
 	//Parse arguments
-	const zip = new Zip({});
-	switch (args[0]) {
+	const argIndex = 0;
+	switch (args[argIndex]) {
 		case "store":
-			if (argsLen < 2) {
+			const storeNumArgs = 2 + argIndex;
+			if (argsLen < storeNumArgs) {
 				throw new Error("Store key required");
 			}
-			const storeKey = args[1];
-			const storeVal = argsLen > 2 ? args.slice(2).join(" ") : await _prompt(`Please enter the value for "${storeKey}"`);
+			const storeKey = args[argIndex + 1];
+			const storeVal = argsLen > storeNumArgs ? args.slice(storeNumArgs).join(" ") : await _prompt(`Please enter the value for "${storeKey}"`);
 			zip.store(storeKey, storeVal);
+			await _writeFile(filePath, await zip.getStream());
 			break;
 
 		case "view":
+			if (argsLen < 2 + argIndex) {
+				throw new Error("View key required");
+			}
+			const viewKey = args[argIndex + 1];
+			console.log(await zip.retrieve(viewKey));
 			break;
 
 		case "open":
+			if (argsLen < 2 + argIndex) {
+				throw new Error("Open key required");
+			}
+			const openKey = args[argIndex + 1];
+			const openFile = openKey.replace(FILENAME_REGEX, "$1");
+			await fs.promises.writeFile(zip.ensureExtension(openFile), await zip.retrieve(openKey));
+			console.log("TODO: Spawn child process");
 			break;
 
 		case "list":
+			const entries = await zip.list();
+			entries.map((entry) => console.log(entry));
 			break;
 
 		case "delete":
+			if (argsLen < 2 + argIndex) {
+				throw new Error("Delete key required");
+			}
+			const deleteKey = args[argIndex + 1];
+			await zip.delete(deleteKey);
+			await _writeFile(filePath, await zip.getStream());
 			break;
 
 		case "import":
+			const importNumArgs = 2 + argIndex;
+			if (argsLen < importNumArgs) {
+				throw new Error("File required");
+			}
+			const importFile = args[argIndex + 1];
+			const importKey = argsLen > importNumArgs ? args[argIndex + 2] : importFile.replace(FILENAME_REGEX, "$1");
+			const importData = await fs.promises.readFile(importFile);
+			zip.store(importKey, importData);
+			await _writeFile(filePath, await zip.getStream());
 			break;
 
 		case "export":
+			const exportNumArgs = 2 + argIndex;
+			if (argsLen < exportNumArgs) {
+				throw new Error("Export key required");
+			}
+			const exportKey = args[argIndex + 1];
+			const exportFile = argsLen > exportNumArgs ? args[argIndex + 2] : exportKey.replace(FILENAME_REGEX, "$1");
+			await fs.promises.writeFile(zip.ensureExtension(exportFile), await zip.retrieve(exportKey));
 			break;
 
 		case "exportall":
+			if (argsLen < 2 + argIndex) {
+				throw new Error("File required");
+			}
+			const exportAllFile = args[argIndex + 1];
+			console.log("TODO: Export data");
 			break;
 
 		case "passwd":
+			console.log("TODO: Change password");
 			break;
 
 		case "?":
@@ -82,15 +136,37 @@ async function run() {
 function _showCommands() {
 	console.log(`${packageName} ${packageVersion}`);
 	console.log("");
-	console.log("Usage examples:");
-	console.log(`    ${packageName} store seed/helium abcdefg`);
-	console.log(`    ${packageName} view seed/helium`);
-	console.log(`    ${packageName} open seed/helium`);
+	console.log("Store seed phrase or keys");
+	console.log(`    ${packageName} store ${EXAMPLE_NAME}`);
+	console.log(`    ${packageName} store ${EXAMPLE_NAME} this is my ${EXAMPLE_NAME} seed phrase`);
+	console.log(`    ${packageName} store seed/${EXAMPLE_NAME} this is my ${EXAMPLE_NAME} seed phrase`);
+	console.log("");
+	console.log("View seed phrase or key in console");
+	console.log(`    ${packageName} view ${EXAMPLE_NAME}`);
+	console.log(`    ${packageName} view seed/${EXAMPLE_NAME}`);
+	console.log("");
+	console.log("Open seed phrase or key with file system default");
+	console.log(`    ${packageName} open ${EXAMPLE_NAME}`);
+	console.log(`    ${packageName} open seed/${EXAMPLE_NAME}`);
+	console.log("");
+	console.log("List stores");
 	console.log(`    ${packageName} list`);
-	console.log(`    ${packageName} delete seed/helium`);
-	console.log(`    ${packageName} import ./ravencoin.txt seed/ravencoin`);
-	console.log(`    ${packageName} export seed/ravencoin ./ravencoin.txt`);
-	console.log(`    ${packageName} exportall ./${packageName}-data/`);
+	console.log("");
+	console.log("Delete store(s)");
+	console.log(`    ${packageName} delete ${EXAMPLE_NAME}`);
+	console.log(`    ${packageName} delete seed/${EXAMPLE_NAME}`);
+	console.log(`    ${packageName} delete seed`);
+	console.log("");
+	console.log("Import file");
+	console.log(`    ${packageName} import ./${EXAMPLE_NAME}.txt`);
+	console.log(`    ${packageName} import ./${EXAMPLE_NAME}.txt seed/${EXAMPLE_NAME}`);
+	console.log("");
+	console.log("Export file(s)");
+	console.log(`    ${packageName} export ${EXAMPLE_NAME}`);
+	console.log(`    ${packageName} export seed/${EXAMPLE_NAME} ./${EXAMPLE_NAME}.txt`);
+	console.log(`    ${packageName} exportall ./${packageName}-data`);
+	console.log("");
+	console.log("Change password");
 	console.log(`    ${packageName} passwd`);
 	console.log("");
 }
@@ -111,12 +187,21 @@ function _prompt(str) {
 	});
 }
 
+function _writeFile(filePath, content) {
+	//TODO: Encrypt content
+	return fs.promises.writeFile(filePath, content);
+}
+
 (async function execute() {
 	try {
 		const result = await run();
 		process.exit(result);
 	} catch (err) {
-		console.error(`ERROR: ${err.message}`);
+		if (DEBUG) {
+			console.error(err);
+		} else {
+			console.error(`ERROR: ${err.message}`);
+		}
 		process.exit(1);
 	}
 })();
--- src/zip.js
+++ src/zip.js
@@ -1,10 +1,66 @@
+const JSZip = require("jszip");
+
 class _class {
-	constructor(options) {
-		this.options = options;
+	constructor({debug = false}) {
+		this.debug = debug;
+		this.zip = new JSZip();
 	}
 
 	store(key, value) {
-		console.log("Incypher::store", key, value);
+		if (this.debug) {
+			console.log("zip::store", key, value);
+		}
+		this.zip.file(this.ensureExtension(key), value);
+	}
+
+	retrieve(key, type = "string") {
+		if (this.debug) {
+			console.log("zip::retrieve", key, type);
+		}
+		return this.zip.file(this.ensureExtension(key)).async(type);
+	}
+
+	list() {
+		if (this.debug) {
+			console.log("zip::list");
+		}
+		const entries = [];
+		this.zip.forEach((key, info) => {
+			if (!info.dir) {
+				//Remove txt file extension
+				const entry = key.replace(/\.txt$/, "");
+				entries.push(entry);
+			}
+		});
+		return entries;
+	}
+
+	delete(key) {
+		if (this.debug) {
+			console.log("zip::delete", key);
+		}
+		return this.zip.remove(this.ensureExtension(key));
+	}
+
+	setStream(stream) {
+		if (this.debug) {
+			console.log("zip::setStream");
+		}
+		return this.zip.loadAsync(stream);
+	}
+	getStream() {
+		if (this.debug) {
+			console.log("zip::getStream");
+		}
+		return this.zip.generateAsync({type: "nodebuffer"});
+	}
+
+	//Ensure our fileName has a file extension
+	ensureExtension(fileName) {
+		if (!/\..+$/.test(fileName)) {
+			return `${fileName}.txt`;
+		}
+		return fileName;
 	}
 }
 module.exports = _class;
